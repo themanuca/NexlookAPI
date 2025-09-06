6338b57eafcb51371eaaff3774315bdb78dc69d8
Sergio Manuca <ssergioca@gmail.com>
2025-09-05 21:20:30 -0300

    Criação da camada Infra. Configuração do dbcontext e conexão com db

---
+++ NexLookSolution/Infra/dbContext/AppDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.dbContext
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Look> Looks { get; set; }
+        public DbSet<LookImage> LookImages { get; set; }
+        public DbSet<Credito> Creditos { get; set; }
+        public DbSet<Subscricao> Subscricoes { get; set; }
+        public DbSet<Plano> Planos { get; set; }
+        public DbSet<Pagamento> Pagamentos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Usuario - Subscricao
+            modelBuilder.Entity<Subscricao>()
+                .HasOne(s => s.Usuario)
+                .WithMany(u => u.Subscriptions)
+                .HasForeignKey(s => s.UsuarioId);
+
+            // Usuario - Look
+            modelBuilder.Entity<Look>()
+                .HasOne(l => l.Usuario)
+                .WithMany(u => u.Looks)
+                .HasForeignKey(l => l.UsuarioId);
+
+            // Usuario - Credito
+            modelBuilder.Entity<Credito>()
+                .HasOne(c => c.Usuario)
+                .WithMany()
+                .HasForeignKey(c => c.UsuarioId);
+
+            // Subscricao - Plano
+            modelBuilder.Entity<Subscricao>()
+                .HasOne(s => s.Plano)
+                .WithMany(p => p.Subscriptions)
+                .HasForeignKey(s => s.PlanoId);
+
+            // Subscricao - Pagamento
+            modelBuilder.Entity<Pagamento>()
+                .HasOne(p => p.Subscricao)
+                .WithMany(s => s.Payments)
+                .HasForeignKey(p => p.SubscricaoId);
+
+            // Look - LookImage
+            modelBuilder.Entity<LookImage>()
+                .HasOne(li => li.Look)
+                .WithMany(l => l.Images)
+                .HasForeignKey(li => li.LookId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
